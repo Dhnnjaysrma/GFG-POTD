33f6f7280472a5eed05fdbb01125e709be2eda67
Dhananjay Sharma <113139387+Dhnnjaysrma@users.noreply.github.com>
2024-04-17 19:48:03 +0530

    Update 17 April 2024.cpp

--- 04-April(2024)/17 April 2024.cpp
+++ 04-April(2024)/17 April 2024.cpp
@@ -1,3 +1,12 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> orderedMultiSet;
+
+
+
 class Solution {
 public:
     int countPairs(int arr[], int n) {
